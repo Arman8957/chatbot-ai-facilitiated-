148d0b1d5f34395b674b92e12c4ed9b3a24dc426
Arman8957 <novicethought0@gmail.com>
2025-05-17 22:05:28 +0600

    conflict updated

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,13 @@
+import 'package:chatbot_ai_facilitated/home_screen.dart';
 import 'package:flutter/material.dart';
 
 
+class LiveScore extends StatelessWidget {
+  const LiveScore({super.key});
+
   @override
   Widget build(BuildContext context) {
-    );
+    return const HomeScreen();
   }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:chatbot_ai_facilitated/app.dart';
 import 'package:flutter/material.dart';
 
 
+void main() {
+  runApp(const LiveScore());
 }
